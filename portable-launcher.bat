dd92926d46ea01782cb9b6e37476c370e150eec5
CraZyDuDe <83429736+TheCraZyDuDee@users.noreply.github.com>
2024-04-27 15:49:19 +0200

    Update portable-launcher.bat
    added the option to choose between downloader and changed plutonium file path

--- portable-launcher.bat
+++ portable-launcher.bat
@@ -1,18 +1,56 @@
 @echo off
 title Plutonium Portable Launcher
-if exist plutonium-updater.exe (goto start) else (goto download)
+:checkdownloader
+if exist plutonium-updater.exe (goto start2) else (if exist plutonium.exe (goto start) else (goto downloaderchoice))
+:downloaderchoice
+cls
+echo.
+echo Which downloader would you like to use in the future?
+echo (this will only show up the first time when no downloader is found)
+echo.
+echo 1 = Official Plutonium Installer
+echo 2 = mxve's plutonium-updater.js
+echo.
+echo if unsure just select the official one.
+echo.
+set /p c=Select your Option: 
+if "%c%"=="1" goto download
+if "%c%"=="2" goto download2
+if "%c%" GTR "2" goto downloaderchoice
+if "%c%" LSS "1" goto downloaderchoice
 :download
+cls
+echo.
+echo Downloading Updater...
+echo.
+powershell -Command "Start-BitsTransfer "https://cdn.plutonium.pw/updater/plutonium.exe"
+echo Done!
+goto start
+:download2
+cls
+echo.
 echo Downloading Updater...
 echo.
 powershell -Command "Start-BitsTransfer "https://github.com/mxve/plutonium-updater.rs/releases/latest/download/plutonium-updater-x86_64-pc-windows-msvc.zip"
 tar -xf plutonium-updater-x86_64-pc-windows-msvc.zip
 del plutonium-updater-x86_64-pc-windows-msvc.zip
 echo Done!
-goto start
+goto start2
 :start
-if exist "bin\plutonium-launcher-win32.exe" (echo Updating Plutonium Portable...) else (echo Downloading Plutonium Files...)
+cls
 echo.
-plutonium-updater.exe -l -d "%~dp0"
+if exist "plutonium\bin\plutonium-launcher-win32.exe" (echo Updating Plutonium Files...) else (echo Downloading Plutonium Files...)
+plutonium.exe -install-dir "plutonium"
+echo.
+echo Update finished! Exiting...
+timeout /t 3 >nul
+exit
+:start2
+cls
+echo.
+if exist "plutonium\bin\plutonium-launcher-win32.exe" (echo Updating Plutonium Files...) else (echo Downloading Plutonium Files...)
+echo.
+plutonium-updater.exe -l -d "%~dp0\plutonium"
 start "" "bin\plutonium-launcher-win32.exe" >nul
 echo.
 echo Update finished! Exiting...
